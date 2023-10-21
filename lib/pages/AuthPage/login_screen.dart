b83806d61e18fcb5f8f3ac1c8dcbec11bc323648
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-10-21 22:17:22 +0900

    Login 관련 UI 완료
    기능은 추가하지않았습니다 .!

---
+++ lib/pages/AuthPage/login_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:hakgeun_market/pages/AuthPage/regist_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white, // AppBar의 배경색을 흰색으로 설정
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black), // 아이콘 색상 설정
+          onPressed: () {
+            Navigator.of(context).pop(); // 뒤로 가기 아이콘 동작
+          },
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10.0),
+            const Text(
+              "안녕하세요!",
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+            const SizedBox(height: 7.0),
+            const Text(
+              "휴대폰번호로 로그인해주세요.",
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "휴대폰번호",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey, // 회색 테두리
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "비밀번호",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey, // 회색 테두리
+                  ),
+                ),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              children: [
+                const Text(
+                  "학근마켓이 처음이신가요?",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // 회원가입 페이지로 이동하는 코드를 여기에 추가
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RegistScreen()));
+                  },
+                  child: const Text(
+                    "회원가입",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {
+                // 로그인 버튼을 클릭했을 때의 동작을 여기에 추가
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF2DB400),
+              ),
+              child: const SizedBox(
+                width: double.infinity,
+                height: 50.0,
+                child: Center(
+                  child: Text(
+                    "로그인",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/AuthPage/main_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:hakgeun_market/pages/AuthPage/login_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.school,
+              size: 200.0,
+              color: Color(0xFF2DB400),
+            ),
+            const Text(
+              "학근마켓",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              "학교내 중고거래부터 학식메이트까지\n지금 내 학교를 검색하고 시작해보세요.",
+              style: TextStyle(
+                color: Color(0xFF8C8C8C),
+                fontSize: 18.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20.0),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                // 가로로 쭉 늘리기
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 40.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 버튼이 클릭될 때의 동작 정의
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF2DB400),
+                    ),
+                    child: const Text(
+                      "시작하기",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
