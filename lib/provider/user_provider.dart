0c59fda83a91e67afe5971346d6efe87363f1cf9
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-11-22 17:28:01 +0900

    set user 수정

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import '../models/user.dart';
 
-class UserProvider with ChangeNotifier {
+class UserProvider extends ChangeNotifier {
+  static final UserProvider _instance = UserProvider._internal();
+
+  factory UserProvider() {
+    return _instance;
+  }
+
+  UserProvider._internal();
+
   UserModel? _user;
 
   UserModel? get user => _user;
 
-  void setUser(UserModel user) {
+  void setUser(UserModel? user) {
     _user = user;
-    notifyListeners();
+    notifyListeners(); // 상태 변경 알림
   }
 }
